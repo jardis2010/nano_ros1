2bdd5687fcd5f61df4fab4b97df1684609dff337
jardis2010 <janez.ambrozic@siol.net>
2023-09-19 14:39:03 +0200

    mod: update dockerfile and install_... to have a (more) complete system

--- docker/install_ros_melodic_et_al.sh
+++ docker/install_ros_melodic_et_al.sh
@@ -29,6 +29,17 @@ curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key
 # 
 # install ROS packages
 #
+
+# TODO - ros-melodic-full option would be better
+# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+# apt update
+# apt install ros-melodic-desktop-full
+# apt-get update && \
+#   apt-get install -y --no-install-recommends \
+#   ros-melodic-map-server
+
+
 apt-get update && \
     apt-get install -y --no-install-recommends \
 		ros-melodic-`echo "${ROS_PKG}" | tr '_' '-'` \
@@ -42,8 +53,16 @@ apt-get update && \
         ros-melodic-rviz \
         ros-melodic-urdf \
         ros-melodic-xacro \
+        ros-melodic-rqt \
+        ros-melodic-rqt-common-plugins \
+        ros-melodic-joint-state-publisher \
+        ros-melodic-joint-state-publisher-gui \
+        ros-melodic-robot-state-publisher \
+        ros-melodic-laser-filters \
+        ros-melodic-gmapping \
         libgazebo9 \
         ros-melodic-gazebo-ros \
+        ros-melodic-gazebo-ros-pkgs \
         python-rosdep \
         python-rosinstall \
         python-rosinstall-generator \
